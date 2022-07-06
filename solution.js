25cd6219ff40a0cca7715aa8ced2f16bb6fd138c
Boi Schrader <boi1992@t-online.de>
2022-07-06 15:31:59 +0200

    operators done after issues

---
+++ solution.js
@@ -0,0 +1,23 @@
+// 1.
+
+const isDogBetter = true;
+const isCatBetter = false;
+
+// 1. a)
+
+console.log(isDogBetter && isCatBetter);
+console.log(isDogBetter || isCatBetter);
+console.log(!(isDogBetter && isCatBetter));
+
+// 2
+const atoms = 530;
+const starsInSky = 350;
+const sandGrains = 865;
+
+// 3
+console.log(atoms > starsInSky && atoms > sandGrains);
+console.log(atoms !== sandGrains);
+console.log(starsInSky < sandGrains || starsInSky > atoms)
+console.log(atoms == starsInSky || atoms !== sandGrains)
+console.log(atoms >= 10 && sandGrains <= 10 )
+console.log(atoms * starsInSky < 100 || atoms * sandGrains > 100)
